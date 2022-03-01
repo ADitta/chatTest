fd5701ce5dc8381ba622332175ce49ded179d39f
Bezll <jabez1992@hotmail.com>
2022-03-01 11:29:17 +0000

    Live share to use IOS

--- App.tsx
+++ App.tsx
@@ -9,12 +9,14 @@ import Navigation from "./src/navigation";
 import { StreamChat } from "stream-chat";
 import { useEffect, useState } from "react";
 import {
-  OverlayProvider,
-  Chat,
-  ChannelList,
-  Channel,
-  DeepPartial,
-  Theme,
+	OverlayProvider,
+	Chat,
+	ChannelList,
+	Channel,
+	DeepPartial,
+	Theme,
+	MessageList,
+	MessageInput,
 } from "stream-chat-expo";
 import { Text } from "react-native";
 import { StreamColors } from "./src/constants/Colors";
@@ -23,71 +25,73 @@ const API_KEY = "556hs76mkyz2";
 const client = StreamChat.getInstance(API_KEY);
 
 const theme: DeepPartial<Theme> = {
-  colors: StreamColors,
+	colors: StreamColors,
 };
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-  const [isReady, setIsReady] = useState(false);
-  const [selectedChannel, setSelectedChannel] = useState(null);
-  const connectUser = async () => {
-    // SIgn in with bakend and get user token, link it in connectUser
-    await client.connectUser(
-      {
-        id: "Asim",
-        name: "Asim",
-        image: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
-      },
-      client.devToken("Asim")
-    );
-    setIsReady(true);
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
+	const [isReady, setIsReady] = useState(false);
+	const [selectedChannel, setSelectedChannel] = useState(null);
+	const connectUser = async () => {
+		// SIgn in with bakend and get user token, link it in connectUser
+		await client.connectUser(
+			{
+				id: "Asim",
+				name: "Asim",
+				image: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+			},
+			client.devToken("Asim")
+		);
+		setIsReady(true);
 
-    // Create a channel
-    // const channel = client.channel("team", "general", { name: "General" });
+		// Create a channel
+		// const channel = client.channel("team", "general", { name: "General" });
 
-    // await channel.create();
-  };
+		// await channel.create();
+	};
 
-  useEffect(() => {
-    connectUser();
-  }, []);
+	useEffect(() => {
+		connectUser();
+	}, []);
 
-  const onChannelSelect = (channel) => {
-    console.log(channel);
-    console.log("hi");
-    setSelectedChannel(channel);
-  };
+	const onChannelSelect = (channel) => {
+		setSelectedChannel(channel);
+	};
 
-  if (!isLoadingComplete || !isReady) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <OverlayProvider>
-          <Chat client={client}>
-            {/* <Navigation colorScheme={colorScheme} /> */}
-            {!selectedChannel ? (
-              <ChannelList
-                onSelect={(channel) => {
-                  setSelectedChannel(channel);
-                }}
-              />
-            ) : (
-              <>
-                <Channel channel={selectedChannel}>
-                  <Text
-                    style={{ margin: 20, backgroundColor: "red", fontSize: 50 }}
-                    onPress={onChannelSelect}
-                  >
-                    Go Back
-                  </Text>
-                </Channel>
-              </>
-            )}
-          </Chat>
-        </OverlayProvider>
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+	if (!isLoadingComplete || !isReady) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+				<OverlayProvider>
+					<Chat client={client}>
+						{/* <Navigation colorScheme={colorScheme} /> */}
+						{!selectedChannel ? (
+							<ChannelList
+								onSelect={(channel) => {
+									setSelectedChannel(channel);
+								}}
+							/>
+						) : (
+							<>
+								<Channel channel={selectedChannel}>
+									<Text
+										style={{
+											margin: 40,
+										}}
+										onPress={() => setSelectedChannel(null)}
+									>
+										Go Back
+									</Text>
+									<MessageList />
+									<MessageInput />
+								</Channel>
+							</>
+						)}
+					</Chat>
+				</OverlayProvider>
+				<StatusBar />
+			</SafeAreaProvider>
+		);
+	}
 }
