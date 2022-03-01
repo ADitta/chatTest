77576d88856fe18920bdd07d40d1690ed3439791
= <=>
2022-03-01 10:15:33 +0000

    add everything

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
+import Navigation from "./src/navigation";
 
+import { StreamChat } from "stream-chat";
+import { useEffect, useState } from "react";
+import {
+  OverlayProvider,
+  Chat,
+  ChannelList,
+  Channel,
+  DeepPartial,
+  Theme,
+} from "stream-chat-expo";
+import { Text } from "react-native";
+import { StreamColors } from "./src/constants/Colors";
+
+const API_KEY = "556hs76mkyz2";
+const client = StreamChat.getInstance(API_KEY);
+
+const theme: DeepPartial<Theme> = {
+  colors: StreamColors,
+};
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [isReady, setIsReady] = useState(false);
+  const [selectedChannel, setSelectedChannel] = useState(null);
+  const connectUser = async () => {
+    // SIgn in with bakend and get user token, link it in connectUser
+    await client.connectUser(
+      {
+        id: "Asim",
+        name: "Asim",
+        image: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+      },
+      client.devToken("Asim")
+    );
+    setIsReady(true);
 
-  if (!isLoadingComplete) {
+    // Create a channel
+    // const channel = client.channel("team", "general", { name: "General" });
+
+    // await channel.create();
+  };
+
+  useEffect(() => {
+    connectUser();
+  }, []);
+
+  const onChannelSelect = (channel) => {
+    console.log(channel);
+    console.log("hi");
+    setSelectedChannel(channel);
+  };
+
+  if (!isLoadingComplete || !isReady) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider>
+          <Chat client={client}>
+            {/* <Navigation colorScheme={colorScheme} /> */}
+            {!selectedChannel ? (
+              <ChannelList
+                onSelect={(channel) => {
+                  setSelectedChannel(channel);
+                }}
+              />
+            ) : (
+              <>
+                <Channel channel={selectedChannel}>
+                  <Text
+                    style={{ margin: 20, backgroundColor: "red", fontSize: 50 }}
+                    onPress={onChannelSelect}
+                  >
+                    Go Back
+                  </Text>
+                </Channel>
+              </>
+            )}
+          </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
